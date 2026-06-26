37e21e5733a60dc15084cf7d61d4c32f6826108f
Khaled Yasser <khaledyasser1234@yahoo.com>
2026-06-26 20:59:41 +0300

    updated files

--- CollegeSystem/Controllers/HomeController.cs
+++ CollegeSystem/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CollegeSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CollegeSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View("Index");
+            var courses = _context.Courses
+        .Include(c => c.Professor)
+        .ToList();
+
+            var professors = _context.Professors.ToList();
+
+            ViewBag.Courses = courses;
+            ViewBag.Professors = professors;
+
+            return View();
         }
     }
 }
